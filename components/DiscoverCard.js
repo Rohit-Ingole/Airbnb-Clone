22429fc734013b75784bf8335d72b407f1c339ca
Rohit-Ingole <rvingole68@gmail.com>
2021-08-05 19:14:57 +0530

    made it better

--- components/DiscoverCard.js
+++ components/DiscoverCard.js
@@ -6,8 +6,8 @@ const DiscoverCard = ({ image, title, desc }) => {
       <div className="relative h-[18rem] w-[18rem] md:h-[22rem] md:w-[22rem] mb-5">
         <Image src={image} layout="fill" className="rounded-lg" />
       </div>
-      <h3 className="text-sm md:text-lg font-semibold">{title}</h3>
-      <h4 className="text-xs md:text-md">{desc}</h4>
+      <h3 className="text-md md:text-lg font-semibold">{title}</h3>
+      <h4 className="text-sm md:text-md">{desc}</h4>
     </div>
   );
 };
