e45eca49daf68f4b62e09a5e268e215214d29f9f
Rohit-Ingole <rvingole68@gmail.com>
2021-08-05 19:03:32 +0530

    made it better

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const MediumCard = ({ image, title }) => {
   return (
-    <div className="cursor-pointer hover:scale-105 transition transform duration-300 ease-out ">
-      <div className="relative h-64 w-64 display">
+    <div className="cursor-pointer hover:scale-105 transition transform duration-300 ease-out">
+      <div className="relative h-64 w-64 display mb-2">
         <Image src={image} layout="fill" className="rounded-xl" />
       </div>
       <h3 className="text-md">{title}</h3>
