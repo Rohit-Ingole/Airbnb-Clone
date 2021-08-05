2c4a913965ff0f208d2acbd0d924fac67c693f50
Rohit-Ingole <rvingole68@gmail.com>
2021-08-05 19:00:31 +0530

    made it better

--- components/DiscoverCard.js
+++ components/DiscoverCard.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 const DiscoverCard = ({ image, title, desc }) => {
   return (
     <div>
-      <div className="relative h-[22rem] w-[22rem] mb-5">
+      <div className="relative h-[15rem] w-[15rem] md:h-[22rem] md:w-[22rem] mb-5">
         <Image src={image} layout="fill" className="rounded-lg" />
       </div>
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <h4 className="text-md">{desc}</h4>
+      <h3 className="text-sm md:text-lg font-semibold">{title}</h3>
+      <h4 className="text-xs md:text-md">{desc}</h4>
     </div>
   );
 };
