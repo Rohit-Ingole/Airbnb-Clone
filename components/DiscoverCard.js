32a01fb67a4cd66eee99da2f2761879b48370ad8
Rohit-Ingole <rvingole68@gmail.com>
2021-08-05 19:02:17 +0530

    made it better

--- components/DiscoverCard.js
+++ components/DiscoverCard.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const DiscoverCard = ({ image, title, desc }) => {
   return (
-    <div>
-      <div className="relative h-[15rem] w-[15rem] md:h-[22rem] md:w-[22rem] mb-5">
+    <div className="hover:scale-105 transition transform duration-300 ease-out cursor-pointer">
+      <div className="relative h-[18rem] w-[18rem] md:h-[22rem] md:w-[22rem] mb-5">
         <Image src={image} layout="fill" className="rounded-lg" />
       </div>
       <h3 className="text-sm md:text-lg font-semibold">{title}</h3>
